f865405774820e59c99f35016eaf3bb9ca3d403c
Joo <rudwn3166@naver.com>
2021-09-06 20:33:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/intent_test/Change_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.intent_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Change_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change)
+    }
+}
